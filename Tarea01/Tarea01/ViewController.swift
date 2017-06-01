35be87fb6df059d2de20de7609f89c68f2e23e50
academiamoviles <academiamoviles@academiamoviless-MacBook-Pro.local>
2017-05-31 21:04:55 -0500

    todos

---
+++ Tarea01/Tarea01/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Tarea01
+//
+//  Created by academiamoviles on 5/31/17.
+//  Copyright © 2017 Academia moviles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func isValidEmail(testStr:String) -> Bool {
+        
+        print("validate emilId: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let result = emailTest.evaluate(with: testStr)
+        return result
+        
+    }
+
+}
+
